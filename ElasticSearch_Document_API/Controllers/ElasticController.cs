f3a22e58400402875f680b1a308ed13a990e9c94
ITQ <ITQ@LAPTOP-2TTQRMQR>
2022-02-08 11:53:19 +0300

    Добавьте файлы проекта.

---
+++ ElasticSearch_Document_API/Controllers/ElasticController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticSearch_Document_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ElasticController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello!";
+        }
+    }
+}
